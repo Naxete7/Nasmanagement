a5569b585018ac3eb21393aba245de6d55a93ee1
Ignacio <naxete7@hotmail.es>
2024-08-21 21:59:33 +0200

    home casi lista

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,41 +1,31 @@
 import React from "react";
 import "./Header.css";
-
+import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import logo from "../../assets/img/logo.png";
+import { Col, Row } from "react-bootstrap";
 
 function ColorSchemesExample() {
   return (
     <>
-      <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home"></Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">ÑasManagement</Nav.Link>
-           
-          </Nav>
-        </Container>
-      </Navbar>
-      <br />
-      <Navbar bg="primary" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home"></Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Mentorias grupales</Nav.Link>
-            
-          </Nav>
-        </Container>
-      </Navbar>
-
-      <br />
-      <Navbar bg="light" data-bs-theme="light">
-        <Container>
-          <Navbar.Brand href="#home"></Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Sobre nosotros</Nav.Link>
-           
-          </Nav>
+      <Navbar className="bg-body-tertiary navbarDesign sticky-top">
+        <Container className="sticky-top">
+          <Navbar.Brand href="#home">
+            <Container>
+              <Row className="rowDesign sticky-top">
+                <img
+                  alt=""
+                  src={logo}
+                  className="d-inline-block align-top logoDesign"
+                />{" "}
+                <Col>PortFolio</Col>
+                <Col>PortFolio</Col>
+                <Col>PortFolio</Col>
+              </Row>
+            </Container>
+          </Navbar.Brand>
         </Container>
       </Navbar>
     </>
--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 import "./Home.css"
+import { Col, Container, Row } from "react-bootstrap";
+import { Card } from "react-bootstrap";
+import broja from "../../assets/img/broja.png"
+import Nplumaroja from "../../assets/img/Nplumaroja.png";
+import logonegro from "../../assets/img/logonegro.png";
 
-const Home = () => {
-
-    return (
-        <div className="HomeDesign">
-    Soy el Home
-        </div>
-    );
-    
+function OrderingExample() {
+  return (
+    <Container className="HomeDesign">
+      <Row className="rowDesign">
+        <Col>
+          <img className="imgDesign" src={broja} />
+        </Col>
+        <Col>
+          <img className="imgDesign" src={Nplumaroja} />
+        </Col>
+        <Col>
+          {" "}
+          <img className="imgDesign" src={logonegro} />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default Home;
+export default OrderingExample;
